d4badaa7070e5f061e1daa54414e11c224d996e1
RosaVirginiaTirado <71230040+RosaVirginiaTirado@users.noreply.github.com>
2022-01-09 16:37:09 -0500

    conexion backend en produccion y mostrar usuario

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   
-  bd_url: "http://localhost:8080/api",
+  bd_url: "https://backend-caso2.herokuapp.com/api",
 
   
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  bd_url: "http://localhost:8080/api",
+  bd_url: "https://backend-caso2.herokuapp.com/api",
 };
 
-/*
+/*http://localhost:8080/api
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
